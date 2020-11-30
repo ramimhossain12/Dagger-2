cc00b86a3b7e181b367343598a8fd2b8aa0cc0af
ramimhossain12 <ramimhossain6242@gmail.com>
2020-11-30 22:50:50 +0600

    COMPONENT, PROVISION METHODS & CONSTRUCTOR INJECTION

---
+++ app/src/main/java/com/example/dagger2/Car.java
@@ -0,0 +1,24 @@
+package com.example.dagger2;
+
+import android.nfc.Tag;
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Car {
+
+    private  static  final String TAG = "Car";
+    private Engine engine;
+    private Wheel wheel;
+
+
+    @Inject
+    public Car(Engine engine, Wheel wheel) {
+        this.engine = engine;
+        this.wheel = wheel;
+    }
+
+    public void drive() {
+        Log.d(TAG, "driving...");
+    }
+}
---
+++ app/src/main/java/com/example/dagger2/CarComponent.java
@@ -0,0 +1,9 @@
+package com.example.dagger2;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+    Car getCar();
+
+}
