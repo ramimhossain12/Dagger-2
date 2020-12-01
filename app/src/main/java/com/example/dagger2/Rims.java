c27391805971da270a6cd6c6dcf11c90c17e5d1b
ramimhossain12 <ramimhossain6242@gmail.com>
2020-12-01 21:45:24 +0600

    MODULES & PROVIDER METHODS

---
+++ app/src/main/java/com/example/dagger2/Rims.java
@@ -0,0 +1,4 @@
+package com.example.dagger2;
+
+public class Rims {
+}
---
+++ app/src/main/java/com/example/dagger2/WheelModuls.java
@@ -0,0 +1,10 @@
+package com.example.dagger2;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class WheelModuls {
+
+
+}
