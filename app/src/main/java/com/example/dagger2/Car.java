41e243064445f7bbb71269b3091978583701d356
ramimhossain12 <ramimhossain6242@gmail.com>
2020-12-01 20:32:11 +0600

    METHOD INJECTION

--- app/src/main/java/com/example/dagger2/Car.java
+++ app/src/main/java/com/example/dagger2/Car.java
@@ -7,17 +7,18 @@ import javax.inject.Inject;
 
 public class Car {
 
-    private  static  final String TAG = "Car";
+    private static final String TAG = "Car";
     private Engine engine;
     private Wheel wheel;
-
-
     @Inject
     public Car(Engine engine, Wheel wheel) {
         this.engine = engine;
         this.wheel = wheel;
     }
-
+    @Inject
+    public void enableRemote(Remote remote) {
+        remote.setListener(this);
+    }
     public void drive() {
         Log.d(TAG, "driving...");
     }
---
+++ app/src/main/java/com/example/dagger2/Remote.java
@@ -0,0 +1,16 @@
+package com.example.dagger2;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Remote {
+
+    private static final String TAG = "Car";
+    @Inject
+    public Remote() {
+    }
+    public void setListener(Car car) {
+        Log.d(TAG, "Remote connected");
+    }
+}
