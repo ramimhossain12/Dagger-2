74a3eeed7a16f2da257fd69cba7c1bf8b038fee9
ramimhossain12 <ramimhossain6242@gmail.com>
2020-12-01 20:17:57 +0600

    FIELD INJECTION

--- app/src/main/java/com/example/dagger2/MainActivity.java
+++ app/src/main/java/com/example/dagger2/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
 
-    private  Car car;
+    @Inject    Car car;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         CarComponent component = DaggerCarComponent.create();
-        car = component.getCar();
+       component.inject(this);
         car.drive();
     }
 }
